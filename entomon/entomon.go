96f61090b9cc95cd34c933d8bd56661129fd6bd7
David Roundy <roundyd@physics.oregonstate.edu>
2011-03-24 17:10:17 -0700

    add crude hack to create an issue

--- entomon/entomon.go
+++ entomon/entomon.go
@@ -2,11 +2,56 @@
 package entomon
 
 import (
+	"fmt"
 	"os"
+	"rand"
+	crand "crypto/rand"
+	"time"
 	"bytes"
+	"strings"
 	"path/filepath"
 	"io/ioutil"
 )
+
+func init() {
+	// Here we seed the random number generator.  I take the lazy
+	// expensive route and seed it with a cryptographically strong seed.
+	// It'd be better to use the time and maybe something else local
+	// like hostname and/or user name.  But that'd be more coding.
+	b := make([]byte, 8)
+	crand.Read(b) // ignore output, since there's nothing we can do here
+	var seed int64
+	for _, x := range b {
+		seed = int64(x) + 256*seed
+	}
+	rand.Seed(seed)
+}
+
+var (
+	vowels = []string{"a", "a", "ae", "au",
+		"e", "e", "e", "ea", "ee",
+		"i", "io", "iu", "ie", "iou",
+		"o", "oo", "ou",
+		"u", "uou"}
+	consonants = []string{"b", "c", "ch", "d", "f", "g", "h", "j", "k", "l", "m", "n",
+		"p", "qu",
+		"r", "rd", "rf", "rm", "rn", "rk", "rl", "rj", "rp", "rs", "rt", "rsh", "rth", "rv",
+		"s", "sh", "st", "sk", "sch",
+		"t", "th", "tch",
+		"v", "w", "x", "y", "z", "",
+	}
+	postconsonants = []string{"r", "l", "", "", "", "", "", "", "-", "-"}
+)
+
+func randString() string {
+	out := ""
+	for len(out) < 30 {
+		out = out + consonants[rand.Intn(len(consonants))] +
+			postconsonants[rand.Intn(len(postconsonants))] +
+			vowels[rand.Intn(len(vowels))]
+	}
+	return string(out[:30])
+}
 
 func isEntomonHere() bool {
 	fi, err := os.Stat(".entomon")
@@ -54,4 +99,46 @@ func ProjectName() (name string, err os.Error) {
 	}
 	origd, err := os.Getwd()
 	return filepath.Base(origd), err
+}
+
+func WriteComment(dname, author, text string) os.Error {
+	id := randString()
+	err := os.MkdirAll(dname, 0777)
+	if err != nil {
+		return err
+	}
+	fname := dname + "/" + id
+	comment, err := os.Open(fname, os.O_CREAT+os.O_WRONLY+os.O_EXCL, 0777)
+	if err != nil {
+		return err
+	}
+	defer comment.Close()
+	now := time.Seconds()
+	_, err = fmt.Fprintln(comment, time.SecondsToUTC(now).Format(time.RFC3339))
+	if err != nil {
+		goto cleanup
+	}
+	_, err = fmt.Fprintln(comment, author)
+	if err != nil {
+		goto cleanup
+	}
+	_, err = fmt.Fprintln(comment, text)
+	return err
+cleanup:
+	comment.Close()
+	os.Remove(fname) // We should try to clean up...
+	return err
+}
+
+func NewIssue(author, text string) os.Error {
+	if strings.IndexRune(author, '\n') != -1 {
+		return os.NewError("NewIssue: Invalid character '\\n' in author")
+	}
+	id := randString()
+	err := WriteComment(".entomon/issue/"+id, author, text)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Created issue", id)
+	return nil
 }
--- entomonitor.go
+++ entomonitor.go
@@ -4,11 +4,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
+	"io/ioutil"
 	"entomon"
 	"github.com/droundy/goopt"
 )
 
-var all = goopt.Flag([]string{"-a", "--all"}, []string{"--interactive"}, "hello", "goodbye")
+var action = goopt.Alternatives([]string{"-A", "--action"},
+	[]string{"help", "new-issue"}, "select the action to be performed")
 
 func dieOn(err os.Error) {
 	if err != nil {
@@ -22,4 +25,25 @@ func main() {
 	pname, err := entomon.ProjectName()
 	dieOn(err)
 	fmt.Println("Project name is", pname)
+	if *action == "help" {
+		fmt.Println(goopt.Usage())
+		os.Exit(0)
+	}
+	switch *action {
+	case "new-issue":
+		fmt.Print("What is your name? ")
+		inp, e := bufio.NewReaderSize(os.Stdin, 1)
+		dieOn(e)
+		name, e := inp.ReadString('\n')
+		if len(name) < 1 {
+			name = "\n"
+		}
+		fmt.Print("What is the problem? ")
+		bugtext, _ := ioutil.ReadAll(os.Stdin)
+		fmt.Println("Done here")
+		dieOn(entomon.NewIssue(name[:len(name)-1], string(bugtext)))
+	default:
+		fmt.Println("I should do", *action)
+		os.Exit(1)
+	}
 }
