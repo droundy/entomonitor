156362820a26cf19948a73925bf60510757030fe
David Roundy <roundyd@physics.oregonstate.edu>
2011-03-29 11:24:39 -0700

    cut unused code in entomonview

--- entomonview/entomonview.go
+++ entomonview/entomonview.go
@@ -9,46 +9,6 @@ import (
 
 var port = goopt.Int([]string{"--port"}, 8080, "port on which to serve")
 
-// FIXME: There is no way to make the following return something that
-// is both a gui.Bool and a gui.HasText.
-type labelledcheckbox struct {
-	gui.Widget
-	gui.String
-	gui.Changeable
-	gui.Bool
-}
-func LabelledCheckbox(l string) interface { gui.Widget; gui.String; gui.Changeable; gui.Bool } {
-	cb := gui.Checkbox()
-	label := gui.Text(l)
-	table := gui.Row(cb, label)
-	label.OnClick(func() gui.Refresh {
-		cb.Toggle()
-		return cb.HandleChange()
-	})
-	out := labelledcheckbox{ table, label, cb, cb }
-	return &out
-}
-
-type radiobuttons struct {
-	gui.Widget
-	gui.String
-	gui.Changeable
-}
-	
-func RadioButtons(vs... string) interface{ gui.Widget; gui.String; gui.Changeable } {
-	var bs []interface{ gui.Changeable; gui.Bool; gui.String }
-	var ws []gui.Widget
-	for _,v := range vs {
-		b := gui.RadioButton(v)
-		bs = append(bs, b)
-		ws = append(ws, b)
-	}
-	col := gui.Column(ws...)
-	grp := gui.RadioGroup(bs...)
-	return &radiobuttons{ col, grp, grp }
-}
-
-
 func main() {
 	http.HandleFunc("/style.css", styleServer)
 
