bca09ea1fb7a986943e247297907fda9d08235ed
David Roundy <roundyd@physics.oregonstate.edu>
2011-03-30 09:47:16 -0700

    add missing file

---
+++ entomonview/style.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"http"
+)
+
+func styleServer(c http.ResponseWriter, req *http.Request) {
+	c.SetHeader("Content-Type", "text/css")
+	fmt.Fprint(c, `
+html {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    background: #ffffff;
+    font-family: arial,helvetica,"sans serif";
+    font-size: 12pt;
+}
+h1 {
+font-family: verdana,helvetica,"sans serif";
+font-weight: bold;
+font-size: 16pt;
+}
+h2 { font-family: verdana,helvetica,"sans serif";
+font-weight: bold;
+font-size: 14pt;
+}
+p {
+font-family: arial,helvetica,"sans serif";
+font-size:12pt;
+}
+li {
+  font-family: arial,helvetica,"sans serif";
+  font-size: 12pt;
+}
+a {
+  color: #555599;
+}
+`)
+}
