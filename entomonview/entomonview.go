6581e75ccc356afdc45dc96ff2594a0ae9364002
David Roundy <roundyd@physics.oregonstate.edu>
2011-04-03 13:30:52 -0700

    introduce links between bug list and bug pages.

--- entomonview/entomonview.go
+++ entomonview/entomonview.go
@@ -31,27 +31,29 @@ type PageType struct {
 
 func Page() gui.Widget {
 	var x = new(PageType)
-	x.Widget = BugList()
+	x.Widget = BugList(x)
 	x.OnPath(func() gui.Refresh {
 		p := x.GetPath()[1:]
 		fmt.Println("My path is actually", p)
-		psplit := strings.Split(p,"-",2)
+		psplit := strings.Split(p, "-", 2)
 		if len(psplit) != 2 {
 			fmt.Println("Not a particular bug")
-			return gui.StillClean
+			x.Widget = BugList(x)
+			return gui.NeedsRefresh
 		}
-		bnum,err := strconv.Atoi(psplit[1])
+		bnum, err := strconv.Atoi(psplit[1])
 		if err != nil || len(psplit[0]) == 0 {
 			fmt.Println("Not a particular bug ii")
-			return gui.StillClean
+			x.Widget = BugList(x)
+			return gui.NeedsRefresh
 		}
-		x.Widget = BugPage(έντομο.Type(psplit[0]), bnum)
+		x.Widget = BugPage(x, έντομο.Type(psplit[0]), bnum)
 		return gui.NeedsRefresh
 	})
 	return x
 }
 
-func BugList() gui.Widget {
+func BugList(p gui.HasPath) gui.Widget {
 	bugs := []gui.Widget{gui.Text("This will be a bug browser, some day!")}
 	bl, err := bug.List()
 	if err != nil {
@@ -73,27 +75,42 @@ func BugList() gui.Widget {
 		// }
 		lines := strings.Split(cs[0].Text, "\n", 2)
 		status, _ := b.Attributes["status"]
-		bugtable = append(bugtable, []gui.Widget{
-			gui.Button(bugname), gui.Text(status), gui.Text(cs[0].Date), gui.Text(lines[0])})
+		setpath := func() gui.Refresh { p.SetPath("/" + bugname); return p.HandlePath() }
+		bid := gui.Button(bugname)
+		bid.OnClick(setpath)
+		bstatus := gui.Text(status)
+		bstatus.OnClick(setpath)
+		bdate := gui.Text(cs[0].Date)
+		bdate.OnClick(setpath)
+		btitle := gui.Text(lines[0])
+		btitle.OnClick(setpath)
+		bugtable = append(bugtable, []gui.Widget{bid, bstatus, bdate, btitle})
 	}
 	bugs = append(bugs, gui.Empty(), gui.Empty(), gui.Table(bugtable...))
 	return gui.Column(bugs...)
 }
 
-func BugPage(btype έντομο.Type, bnum int) gui.Widget {
+func BugPage(p gui.HasPath, btype έντομο.Type, bnum int) gui.Widget {
 	bl, err := btype.List()
 	if err != nil {
 		return gui.Text("Error: " + err.String())
 	}
 	if bnum >= len(bl) {
-		return gui.Text(fmt.Sprint("Error: no such ",btype," as number ", bnum))
+		return gui.Text(fmt.Sprint("Error: no such ", btype, " as number ", bnum))
 	}
 	b := bl[bnum]
 	cs, err := b.Comments()
 	if err != nil {
 		return gui.Text("Error: " + err.String())
 	}
-	bugs := []gui.Widget{}
+	listall := gui.Button("List all bugs")
+	listall.OnClick(func() gui.Refresh {
+		p.SetPath("/")
+		return p.HandlePath()
+	})
+	bugs := []gui.Widget{
+		listall,
+	}
 	for _, c := range cs {
 		bugs = append(bugs, gui.Text(c.Author), gui.Text(c.Date), gui.Text(c.Text))
 	}
